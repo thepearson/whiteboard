4b97992d6440be5cf4a0dbb66d3b76f155a34d12
Craig Pearson <thepearson@gmail.com>
2023-03-26 12:01:04 +1300

    Types extracted to typefile

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,4 @@
-export type ColorMap = {
-  r: number,
-  g: number,
-  b: number,
-  a?: number
-}
+import { ColorMap } from "./types"
 
 export const Constants: {
   CANVAS_SIZE: {
---
+++ src/types.ts
@@ -0,0 +1,6 @@
+export type ColorMap = {
+  r: number,
+  g: number,
+  b: number,
+  a?: number
+}
